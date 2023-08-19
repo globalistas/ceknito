e676f929b89e82fffb13a73930adfe04e0c0f991
ceknito <64347618+globalistas@users.noreply.github.com>
2023-08-19 08:57:04 +0200

    Adding posts to recount.py

--- cli/recount.py
+++ cli/recount.py
@@ -1,6 +1,6 @@
 import click
 from flask.cli import AppGroup
-from app.models import Sub, SubSubscriber
+from app.models import Sub, SubSubscriber, SubPost
 
 recount = AppGroup("recount", help="Re-count various internal counters")
 
@@ -30,5 +30,34 @@ def subscribers(save):
             Sub.update(
                 subscribers=SubSubscriber.select()
                 .where((SubSubscriber.sid == sub.sid) & (SubSubscriber.status == 1))
+                .count()
+            ).where(Sub.sid == sub.sid).execute()
+
+
+@recount.command(help="Rebuilds all sub's posts counters")
+@click.option(
+    "--save/--dry-run",
+    default=True,
+    help="Use --save (the default) to fix the counts, or --dry-run to just print them.",
+)
+def posts(save):
+    """Update subscriber counts for all the subs."""
+    if save:
+        print("Name                             Before   After")
+    else:
+        print("Name                            Current Correct")
+
+    subs = Sub.select(Sub.sid, Sub.name, Sub.posts).order_by(Sub.name)
+    for sub in subs:
+        count = (
+            SubPost.select()
+            .where((SubPost.sid == sub.sid) & (SubPost.deleted == 0))
+            .count()
+        )
+        print(f"{sub.name:32}{sub.posts:7}{count:8}")
+        if save:
+            Sub.update(
+                posts=SubPost.select()
+                .where((SubPost.sid == sub.sid) & (SubPost.deleted == 0))
                 .count()
             ).where(Sub.sid == sub.sid).execute()
