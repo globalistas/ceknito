7289485a5d406d1ee9373943068e7c0a03abae1c
ceknito <64347618+globalistas@users.noreply.github.com>
2025-03-16 20:04:55 +0100

    2025 redesign v2.5.1

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -778,7 +778,7 @@ pbodyElements.forEach(pbody => {
             if (this.querySelector('[data-icon="image"], [data-icon="remove"]'))  {
                 redirToPost()
             }
-        } else if (event.target.closest('.post-heading, .author, .nsfw-blur')) {
+        } else if (event.target.closest('.author, .nsfw-blur')) {
             return false;
         } else {
             redirToPost()
